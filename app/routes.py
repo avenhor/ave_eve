0ab22796995c71663bfd7c54f67ac133e678ad3d
Jared McArthur <jared.mcarthur@utexas.edu>
2018-10-06 21:51:37 -0500

    Initial commit with existing code

---
+++ app/routes.py
@@ -0,0 +1,135 @@
+from app import app
+#from app.eve_primary import getContracts
+from flask import Flask,render_template, redirect, request, session, flash
+import requests as r
+import os
+import requests_oauthlib
+from config import Config
+from datetime import datetime
+from app import utility as u
+
+app.secret_key = Config.SECRET_KEY
+app.debug = True
+
+EVEESI = requests_oauthlib.OAuth2Session(Config.CLIENT_ID,
+										scope = Config.SCOPES,
+										redirect_uri = Config.REDIRECT_URI)
+
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
+os.environ['OAUTHLIB_IGNORE_SCOPE_CHANGE'] = '1'
+
+char_id = None
+token = None
+token_timestamp = None
+api_data  = None
+
+# ****************************************************************************
+# Setup to grab login details and tokens
+# TODO: Add refresh check. Tokens should have an expiry.
+#
+
+@app.route('/')
+@app.route('/index')
+def index():
+	return render_template('homepage.html', title='Let\'s Login')
+
+@app.route('/login')
+def login():
+	""" Prompt user to authenticate """
+	auth_base = Config.AUTHORITY_URL + Config.AUTH_ENDPOINT
+	authorization_url, state = EVEESI.authorization_url(auth_base)
+	EVEESI.auth_state = state
+	return redirect(authorization_url)
+
+@app.route('/login/authorized')
+def authorized():
+	""" Handle callback from EVE auth """
+	global token_timestamp
+	#if request.query.state != EVEESI.auth_state:
+	#	raise Exception('State returned to redirect URL does not match!')
+	session['access_token'] = EVEESI.fetch_token(Config.AUTHORITY_URL + Config.TOKEN_ENDPOINT,
+						client_secret = Config.CLIENT_SECRET,
+						authorization_response = request.url)
+	session['token_timestamp'] = datetime.now()
+
+	return redirect('/login/get_char_id')
+#	return redirect('/apicall')
+
+@app.route('/login/get_char_id')
+def get_char_id():
+	global api_data
+	global char_id
+	# maybe don't store session vars in globals...
+	global token
+	token = session.get('access_token')['access_token']
+
+	""" Get the character ID associated with the token """
+	api_data = {'Authorization': 'Bearer ' + session.get('access_token')['access_token'],
+				'Content-Type': 'application/json'}
+	session['char_id'] = EVEESI.get(Config.AUTHORITY_URL + Config.VERIFY_ENDPOINT, headers=api_data).json()
+	char_id = session['char_id'] # think about relying on session vars
+	return redirect('/menu')
+#	return redirect('/apicall')
+
+#
+# END setup section
+# ****************************************************************************
+
+@app.route('/dump')
+def dump():
+	""" Dump a block of data """
+	return render_template('dump.html', data=session, title= "Session Data Dumper")
+
+@app.route('/test')
+def test():
+	""" Test utility module """
+	data = u.getPubContracts(10000031,90000000,100000000,Config,api_data)
+#	data = u.getStructureName(1023425394442,Config,api_data)
+#	flash('Length of returned data: {}'.format(len(data)))
+	return render_template('pub_contracts.html', data=data, title="Utility Testing")
+#	return render_template('dump.html', data=data, title="Utility Testing")
+
+@app.route('/menu')
+def menu():
+	""" Display a menu of options """
+	return render_template('menu.html', title = "Please Choose")
+
+@app.route('/apicall')
+def apicall():
+	""" Verify token was obtained by getting user's public details """
+	endpoint = 'characters/' + str(session['char_id']['CharacterID'])
+	data = EVEESI.get(Config.QUERY_BASE + endpoint, headers=api_data).json()
+	return render_template('apicall.html',
+							graphdata = data,
+							endpoint = endpoint,
+							sample='EVE OAuth2 Flow')
+
+@app.route('/location')
+def location():
+	""" Return current location of player. """
+	endpoint = 'characters/' + str(session['char_id']['CharacterID']) + '/location'
+	flash("Endpoint: {}".format(endpoint))
+	data = EVEESI.get(Config.QUERY_BASE + endpoint, headers=api_data).json()
+	return render_template('apicall.html',
+							graphdata = data,
+							endpoint = endpoint,
+							sample = 'Character Location')
+
+@app.route('/contracts')
+def contracts():
+	""" Return a list of player's contracts """
+	endpoint = 'characters/' + char_id + '/contracts'
+	data = EVEESI.get(Config.QUERY_BASE + endpoint, headers=api_data).json()
+	return render_template('apicall.html',
+							graphdata = data,
+							endpoint = endpoint,
+							sample = 'Character Contracts')
+
+@app.route('/pubcontracts')
+def pubcontracts():
+	""" Return public contracts for given region """
+	endpoint = "contracts/public/10000031/"
+	data = EVEESI.get(Config.QUERY_BASE + endpoint, headers=api_data).json()
+	return render_template('pub_contracts.html',
+							data = data)
---
+++ app/utility.py
@@ -0,0 +1,121 @@
+import requests as r
+from datetime import datetime
+import pickle
+import sys
+
+# These caches will reduce the number of API calls to various endpoints
+id_cache = {} # dict of int: string
+name_cache = {} # dict of string:int
+
+def makePickles():
+	pickle_dict = {'id':id_cache,'name':name_cache}
+	with open('caches.pkl','wb') as f:
+		pickle.dump(pickle_dict,f)
+
+def eatPickles():
+	global id_cache
+	global name_cache
+	
+	with open('caches.pkl','rb') as f:
+		pickle_dict = pickle.load(f)
+		
+	id_cache = pickle_dict['id']
+	name_cache = pickle_dict['name']
+
+def idToName(id, Config, api_data):
+	""" Return a name for a given ID 
+
+	Arguments:
+	id: id value to be looked up
+	Config: config object instantiated in routes.py
+	api_data: header information from routes.py
+	"""
+	global name_cache
+	global id_cache
+	eatPickles()
+
+	# TODO: handle lists of IDs
+	if int(id) in id_cache: # this won't work for lists of IDs
+		print('idToName cache hit')
+		return id_cache[int(id)]
+	api_body = str(id)
+	response = r.post(Config.QUERY_BASE + 'universe/names/', headers=api_data, data = '[' + api_body + ']')
+	data = response.json()[0]
+	name = data['name']
+	id_cache[id] = name
+	if data['category'] == 'solar_system':
+		if name not in system_cache:
+			system_cache[name] = int(id)
+	elif data['category'] == 'character':
+		if name not in name_cache:
+			name_cache[name] = int(id)
+	makePickles()
+	return name
+
+def getStructureName(structure_id, Config, api_data):
+	""" Return structure name for given ID
+
+	Arguments:
+	structure_id: id value to be looked up
+	Config: config object instantiated in routes.py
+	"""
+	global id_cache
+	global name_cache
+	eatPickles()
+	if structure_id not in id_cache or id_cache[structure_id] == '':
+		query = "universe/structures/" + str(structure_id) + "/"
+		data = r.get(Config.QUERY_BASE + query, headers=api_data).json()
+		if 'name' in data.keys():
+			id_cache[structure_id] = data['name']
+			makePickles()
+			return data['name']
+		else:
+			makePickles()
+			return ''
+	else:
+		print("getStructureName cache hit")
+		makePickles()
+		return id_cache[structure_id]
+
+def getPubContracts(region_id, min, max, Config, api_data, print=True, exclude_multiple=True):
+	query = "contracts/public/" + str(region_id) + "/"
+	contracts =  r.get(Config.QUERY_BASE + query, headers=api_data)
+	if print == True:
+		return _printContractDetails(contracts,min,max,Config,api_data,exclude_multiple)
+	else:
+		return contracts
+
+def _getPubContractItems(contract_id,Config,api_data):
+	query = "contracts/public/items/" + str(contract_id) + "/"
+	return r.get(Config.QUERY_BASE + query, headers=api_data)
+
+def _printContractDetails(contract_list,min,max,Config,api_data,exclude_multiple):
+	""" Private method. Accepts a json object output by getPubContracts and prints details """
+	contract_data = []
+	for contract in contract_list.json():
+		current = {}
+		item_dict = {}
+		if (contract['price'] >= min 
+			and contract['price'] <= max 
+			and contract['type'] == 'item_exchange'):
+			items = _getPubContractItems(contract['contract_id'],Config,api_data).json()
+			# this line is to exclude multiple-item contracts
+			if exclude_multiple == True:
+				if len(items) > 1:
+					#print('.',end='')
+					continue
+			try:
+				current['id'] = contract['contract_id']
+				current['struct'] = getStructureName(contract['start_location_id'],Config,api_data)
+				current['title'] = contract['title']
+				current['issuer'] = idToName(contract['issuer_id'],Config,api_data)
+				current['price'] = contract['price']
+				current['date_issued'] = contract['date_issued']
+				for item in items:
+					item_dict[idToName(item['type_id'],Config,api_data)] = item['quantity']
+				current['items'] = item_dict
+				contract_data.append(current)
+			except KeyError as e:
+				print("KeyError: {}".format(e))
+	makePickles()
+	return contract_data
